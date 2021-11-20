b1acaa7ea718134232c4806adf9ef9adc610486f
Vaishnavi Pawar <ms.vaishnavipawar@gmail.com>
2021-11-20 19:07:48 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './Components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import About from './Components/Pages/About';
+import Team from './Components/Pages/Team';
+import Support from './Components/Pages/Support';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/team' component={Team} />
+          <Route path='/support' component={Support} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
